a98b5012440903365ae2f1cc4246423d7e17e0e8
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-01 22:55:36 +0100

    start

--- rc05_events_useStateHook/src/App.js
+++ rc05_events_useStateHook/src/App.js
@@ -1,14 +1,7 @@
-import Events from "./components/1-events/Events";
-import Counter from "./components/2-classComponent/Counter";
-import UseStateCounter from "./components/3-useStateHook/UseStateCounter";
-
 function App() {
   return (
     <div>
-      {/* <Events />
-      <Counter />
-      <Counter count={5} /> */}
-      <UseStateCounter />
+      <h1>eventssss</h1>
     </div>
   );
 }
